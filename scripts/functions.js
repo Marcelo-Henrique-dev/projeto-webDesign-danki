afc4c7aeba462ad16f4076b0b6c17e11c461044e
Marcelo Henrique Rosendo Da Silva <marcelo.henrique@mppe.mp.br>
2024-07-25 13:41:39 -0300

    finalizado

--- scripts/functions.js
+++ scripts/functions.js
@@ -4,3 +4,38 @@ const btnMobile = document.querySelector("i.btnMobile")
 btnMobile.addEventListener("click", ()=>{
     menuMobile.classList.toggle('on')
 })
+
+$('.slider-team').slick({
+    arrows: false,
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 1150,
+            settings: {
+                slidesToShow: 3,
+                infinite: true,
+            }
+        }
+    ]
+})
+
+$('.equipe-circles').slick({
+    arrows: false,
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                infinite: true,
+            }
+        }
+    ]
+})
